adbe247998816d87e93e1e057ad0222be1a7997c
Atharva Yadav <78128129+atharva-yadav@users.noreply.github.com>
2022-06-21 14:42:08 +0530

    Added solution - LeetHub

--- 3. Kth largest element - GFG/3.-kth-largest-element.cpp
+++ 3. Kth largest element - GFG/3.-kth-largest-element.cpp
@@ -12,8 +12,15 @@ class Solution
     //Function to return kth largest element from an array.
     int KthLargest(int arr[], int n, int k) {
         // Your code here
-        sort(arr, arr+n, greater<int>());
-        return arr[k-1];
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for(int i=0; i<n; i++){
+            pq.push(arr[i]);
+            
+            if(pq.size() > k)
+                pq.pop();
+        }
+        return pq.top();
     }
 };
 
